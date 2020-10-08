f70ff5369b8245c24dc9e1539dd90112efcfa7de
Daryan Chan <daryan.j.chan@hotmail.com>
2020-10-07 20:49:34 -0400

    first commit

---
+++ magnific/magnific.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#gallery-center').magnificPopup({
+        type: 'image'
+        // other options
+        gallery: {
+            // options for gallery
+            enabled: true
+        },
+    });
+});
